1394c9fefd51643a2fdeefb4091caa8a638e7af1
Md Arifuzzaman <arif_uap@outlook.com>
2019-09-20 16:13:56 +1000

    first commit

---
+++ BeachResort/Controllers/ServiceController.cs
@@ -0,0 +1,47 @@
+﻿using BeachResort.Models;
+using Sitecore.Data.Items;
+using Sitecore.Mvc.Presentation;
+using Sitecore.Web.UI.WebControls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BeachResort.Controllers
+{
+    public class ServiceController:Controller
+    {
+        public ActionResult Index()
+        {
+            return View("~/Views/Service/Index.cshtml", CreateModel());
+        }
+
+        private static List<ServiceModel> CreateModel()
+        {
+            var item = RenderingContext.Current.ContextItem;
+            Sitecore.Data.Fields.MultilistField refMultilistField = item.Fields["Services"];
+
+            var services = new List<ServiceModel>();
+
+            if (refMultilistField != null)
+            {
+                Item[] items = refMultilistField.GetItems();
+                for(int i=0; i < items.Length; i++)
+                {
+                    var serviceItem = items[i];
+                    var serviceModel = new ServiceModel
+                    {
+                        Title = new HtmlString(FieldRenderer.Render(serviceItem, "Title")),
+                        ServiceImage = new HtmlString(FieldRenderer.Render(serviceItem, "ServiceIcon")),
+                        Info = new HtmlString(FieldRenderer.Render(serviceItem, "ServiceInfo"))
+                    };
+
+                    services.Add(serviceModel);
+                }
+            }
+
+            return services;
+        }
+    }
+}
---
+++ BeachResort/Models/ServiceModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BeachResort.Models
+{
+    public class ServiceModel
+    {
+        public HtmlString Title { get; set; }
+        
+        public HtmlString ServiceImage { get; set; }
+
+        public HtmlString Info { get; set; }
+    }
+}
